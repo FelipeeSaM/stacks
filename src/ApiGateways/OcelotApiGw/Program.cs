82d08319fc4dea006de54b3e75f32f0a098e45a4
Felipe eu mesmo <luiz_sena@hotmail.com>
2026-02-21 16:43:15 -0300

    add serilog no ocelot

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,7 +1,9 @@
+using Common.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace OcelotApiGw
 {
@@ -20,10 +22,15 @@ namespace OcelotApiGw
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 })
-                .ConfigureLogging((hostingContext, loggingbuilder) => {
-                    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    loggingbuilder.AddConsole();
-                    loggingbuilder.AddDebug();
-                });
+                .UseSerilog(SeriLogger.Configure);
+        // substitui o logging padrão do .NET Core pelo Serilog, que é um framework de logging mais avançado e flexível.
+        // Ele permite configurar o logging de forma mais detalhada, incluindo a possibilidade de enviar logs para diferentes destinos
+        // (como arquivos, bancos de dados, etc.) e formatar os logs de maneira personalizada.
+
+        //.ConfigureLogging((hostingContext, loggingbuilder) => {
+        //    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        //    loggingbuilder.AddConsole();
+        //    loggingbuilder.AddDebug();
+        //});
     }
 }
